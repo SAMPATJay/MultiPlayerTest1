4639a40e511862cd015dacce6f64e745418bda94
Jay Sampat <jaydsampat@gmail.com>
2020-01-29 21:42:50 +0100

    First Commit
    Original files of a quick start multiplayer

---
+++ MultiplayerTest1/Assets/Scripts/FingerPrintMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerPrintMovement : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos;
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);  
+        this.gameObject.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Vibrate");
+        Handheld.Vibrate();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Vibrate");
+        Handheld.Vibrate();
+    }
+}
---
+++ MultiplayerTest1/Assets/Scripts/GameSetupController.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameSetupController : MonoBehaviour
+{
+    //public GameObject prefab;
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        GameObject instance= PhotonNetwork.Instantiate("FingerPrint", Vector3.zero, Quaternion.identity);
+        instance.GetComponent<FingerPrintMovement>().enabled=true;
+    }
+
+    
+
+}
